7fcee42491cb1b1cd31b4461d3dcb72a48838522
Suzie Lee <wnsuwls0707@gmail.com>
2021-11-21 04:43:11 +0900

    feat: mainPOST api

--- functions/api/routes/main/mainPOST.js
+++ functions/api/routes/main/mainPOST.js
@@ -2,23 +2,39 @@ const functions = require('firebase-functions');
 const _ = require('lodash');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
-const { userSQL } = require('../../../db');
+const { userSQL, postSQL, ddipSQL } = require('../../../db');
 const db = require('../../../db/db');
 const util = require('../../../lib/util');
 
 module.exports = async (req, res) => {
-  const { name } = req.body;
-  const {  } = req.query;
-  if (!name) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, 'Not enough parameters.' ))
+  const { postId,userId } = req.body;
+  if (!postId||!userId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, 'Not enough parameters.' ))
 
   let client;
 
   try {
     client = await db.connect(req);
-    const user = await userSQL.addUser(client,name)
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_USERS_SUCCESS, user))
+    await client.query('BEGIN');
+
+    const user = await userSQL.getUser(client,userId)
+    let post = await postSQL.getPostById(client,postId)
+    const ddips = await ddipSQL.getDdipsByPostId(client,postId)
+    let currentDdips  =ddips?ddips.length:0
+    if(currentDdips>=post.maxCount){
+        res.status(statusCode.FORBIDDEN).send(util.fail(statusCode.FORBIDDEN, "모든 띱이 완료되었어요 ㅠㅠ\n다음 띱을 노려보세요 !", user))
+    }else if(ddips.find(o=>o.userId===userId)){
+        res.status(statusCode.FORBIDDEN).send(util.fail(statusCode.FORBIDDEN, "이미 참여한 띱이예요!!", user))
+    }else{
+        let ddip = await ddipSQL.addDdip(client, postId,userId)
+        res.status(statusCode.OK).send(util.success(statusCode.OK, "띱 성공!\n우리 사이 띱한 사이", {ddip}))
+        await client.query('COMMIT');
+
+    }
+
 
   } catch (error) {
+    await client.query('ROLLBACK');
+
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
     res.status(500).json({ err: error, userMessage: error.message });
--- functions/db/post.js
+++ functions/db/post.js
@@ -13,4 +13,28 @@ const getAllPosts = async (client) => {
     return convertSnakeToCamel.keysToCamel(rows);
   };
 
-module.exports = { getAllPosts };
+  const getPostById = async (client,postId) => {
+    const { rows } = await client.query(
+      /*sql*/`
+      SELECT * FROM post
+      WHERE is_deleted = false
+      AND post.id = ${postId}
+      `,
+    );
+  
+    return convertSnakeToCamel.keysToCamel(rows[0]);
+  };
+  const updatePostCountById = async (client,postId) => {
+    const { rows } = await client.query(
+      /*sql*/`
+      UPDATE post
+      SET max_count = max_count+1, updated_at = now()
+      WHERE post.id = ${postId}
+      RETURNING *
+      `,
+    );
+  
+    return convertSnakeToCamel.keysToCamel(rows[0]);
+  };
+
+module.exports = { getAllPosts,getPostById,updatePostCountById };
