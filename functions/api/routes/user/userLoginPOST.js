030cf7f4a94606b755a53e852cda2b54cfbba5ea
Suzie Lee <wnsuwls0707@gmail.com>
2021-11-21 00:43:13 +0900

    add: routes setting

---
+++ functions/api/routes/user/userLoginPOST.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const { serializeError } = require('serialize-error');
+const _ = require('lodash');
+
+const db = require('../../../sql/db');
+
+module.exports = async (req, res) => {
+  const {  } = req.body;
+  const {  } = req.query;
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+
+    res.status(200).json({
+      err: false,
+    });
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(500).json({ err: error, userMessage: error.message });
+  } finally {
+    client.release();
+  }
+};
