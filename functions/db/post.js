14e4cd7d09cc940b4e13b58ed253ca228171a00f
Youjung Heo <hujumee27@gmail.com>
2021-11-21 04:24:49 +0900

    fix: db/post.js

--- functions/db/post.js
+++ functions/db/post.js
@@ -1,6 +1,6 @@
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
-<<<<<<< HEAD
+HEAD
 const addPost = async (client, userId, title, hashtagLocation, hashtagTime, maxCount, description, imageUrl) => {
   const { rows } = await client.query(
     /*sql*/`
